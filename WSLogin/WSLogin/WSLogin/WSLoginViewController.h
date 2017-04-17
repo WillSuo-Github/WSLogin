e2afefd9a25a4c04e45b4e38422a9190a0653247
WS <a739002611@outlook.com>
2017-04-17 15:40:16 +0800

    2

---
+++ WSLogin/WSLogin/WSLogin/WSLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  WSLoginViewController.h
+//  WSLogin
+//
+//  Created by WS on 2017/4/17.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSLoginViewController : UIViewController
+
+@end
---
+++ WSLogin/WSLogin/WSLogin/WSLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  WSLoginViewController.m
+//  WSLogin
+//
+//  Created by WS on 2017/4/17.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import "WSLoginViewController.h"
+
+@interface WSLoginViewController ()
+
+@end
+
+@implementation WSLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
